9cc0006ba7b9339e6fa4fa67fb1846d81c0df182
rafal-gorecki <rafal.gorecki@husarion.com>
2023-04-28 12:51:38 +0200

    add git action

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -5,4 +5,9 @@ set -e
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/ros_ws/devel/setup.bash"
 
+# Welcome information
+echo "ZED ROS Docker Image"
+echo "---------------------"
+echo 'ROS distro: ' $ROS_DISTRO
+echo "---------------------"    
 exec "$@"
