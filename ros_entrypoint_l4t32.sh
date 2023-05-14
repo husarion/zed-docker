de6df91d640da778695e6fdb37dc8fdbf79eec62
rafal-gorecki <rafal.gorecki@husarion.com>
2023-05-14 19:17:12 +0200

    rm

--- ros_entrypoint_l4t32.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# setup ros environment
-source "/opt/ros/$ROS_DISTRO/setup.bash"
-catkin_make -DCMAKE_BUILD_TYPE=Release || true
-source "/ros_ws/devel/setup.bash"
-
-exec "$@"
