9242eb1fc848b54ebb7c0abb238d4f0dc64fcf0a
rafal-gorecki <rafal.gorecki@husarion.com>
2023-05-15 14:11:59 +0200

    add dds to entrypoint

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
-# setup ros2 environment
-source "/opt/ros/$ROS_DISTRO/setup.bash" --
-source "/ros2_ws/install/local_setup.bash" --
+output=$(husarnet-dds singleshot) || true
+if [[ "$HUSARNET_DDS_DEBUG" == "TRUE" ]]; then
+  echo "$output"
+fi
+
+# setup ROS2 environment
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+test -f "/ros2_ws/install/setup.bash" && source "/ros2_ws/install/setup.bash"
 
 exec "$@"
