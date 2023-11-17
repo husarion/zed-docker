9748555b86dec825e2feb375d9d68bc1e60ac5c0
rafal-gorecki <rafal.gorecki@husarion.com>
2023-11-17 14:06:12 +0100

    New version of Healthcheck

--- healthcheck.cpp
+++ healthcheck.cpp
@@ -1,25 +1,42 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
-#include "iostream"
-#include "cstdlib"
+#include "fstream"
 
 using namespace std::chrono_literals;
 
-#define TIMEOUT 2s
+#define LOOP_PERIOD 2s
+#define MSG_VALID_TIME 5s
 
-bool msg_received = false;
+std::chrono::steady_clock::time_point last_msg_time;
+
+void write_health_status(const std::string& status)
+{
+  std::ofstream healthFile("/health_status.txt");
+  healthFile << status;
+}
 
 void msg_callback(const sensor_msgs::msg::Image::SharedPtr msg)
 {
   std::cout << "Message received" << std::endl;
-  msg_received = true;
-  rclcpp::shutdown();
+  last_msg_time = std::chrono::steady_clock::now();
 }
 
-void timeout_callback()
+void healthy_check(const rclcpp::Node::SharedPtr& node)
 {
-  std::cout << "Timeout" << std::endl;
-  rclcpp::shutdown();
+  std::chrono::steady_clock::time_point current_time = std::chrono::steady_clock::now();
+  std::chrono::duration<double> elapsed_time = current_time - last_msg_time;
+  bool is_msg_valid = elapsed_time.count() < MSG_VALID_TIME.count();
+
+  if (is_msg_valid)
+  {
+    std::cout << "Health check: healthy" << std::endl;
+    write_health_status("healthy");
+  }
+  else
+  {
+    std::cout << "Health check: unhealthy" << std::endl;
+    write_health_status("unhealthy");
+  }
 }
 
 int main(int argc, char* argv[])
@@ -32,19 +49,23 @@ int main(int argc, char* argv[])
   {
     std::string zed_str(zed);
     zed_str = zed_str.substr(0, zed_str.find('.'));
-    std::string topic_name = "/" + zed_str + "/zed_node/rgb/image_rect_color";
+    std::string topic_name = zed_str + "/zed_node/rgb/image_rect_color";
 
     std::cout << topic_name <<std::endl;
     auto sub = node->create_subscription<sensor_msgs::msg::Image>(topic_name, rclcpp::SensorDataQoS(), msg_callback);
 
-    auto timer = node->create_wall_timer(TIMEOUT, timeout_callback);
-
-    rclcpp::spin(node);
-    return msg_received ? EXIT_SUCCESS : EXIT_FAILURE;
+    while (rclcpp::ok())
+    {
+      rclcpp::spin_some(node);
+      healthy_check(node);
+      std::this_thread::sleep_for(LOOP_PERIOD);
+    }
   }
   else
   {
     std::cerr << "CAMERA_LAUNCH environment variable not set" << std::endl;
-    return EXIT_FAILURE;
+    return 1;
   }
+
+  return 0;
 }
